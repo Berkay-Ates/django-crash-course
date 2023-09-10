31234667ad12e542968ddbf633a8ce50573d390c
Berkay-Ates <atesberkay2356@gmail.com>
2023-09-10 16:11:09 +0300

    django crash course done

---
+++ movies/views.py
@@ -0,0 +1,40 @@
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from .models import Movie
+
+
+def movies(request):
+    data = Movie.objects.all()
+    return render(request, "movies/movies.html", {"movies": data})
+
+
+def home(request):
+    return HttpResponse("this is the home page of the django app")
+
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, "movies/detail.html", {"movie": data})
+
+
+def add(request):
+    title = request.POST.get("title")
+    year = request.POST.get("year")
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect("/movies/")
+
+    return render(
+        request,
+        "movies/add.html",
+    )
+
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404("Movie does not exists")
+    movie.delete()
+    return HttpResponseRedirect("/movies/")
